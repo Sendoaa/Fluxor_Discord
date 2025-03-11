bfca37890e0103566bf62876cd6ab38250661c89
Sendoa Avedillo <sendoave@gmail.com>
2025-03-11 14:09:37 +0100

    GuildVoiceStates añadido

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
   ],
 });
 
